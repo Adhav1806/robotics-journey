0df3b6a0230a0fdce67ceb6e6a505222632155dd
Adhav1806 <adhav1806@gmail.com>
2026-06-27 00:43:28 +0530

    Update servo_radar.ino

--- projects/day4-servo-radar/servo_radar.ino
+++ projects/day4-servo-radar/servo_radar.ino
@@ -9,43 +9,45 @@ float readval;
 int theta;
 unsigned long dur;
 float dist;
+int sweepdir = 1;
 
 void setup() {
-  myServo.attach(thetapin);
-  myServo.write(0);
-  delay(1000);
-  pinMode(trig, OUTPUT);
-  pinMode(echo, INPUT);
-  Serial.begin(9600);
+    Serial.begin(9600);
+    myServo.attach(thetapin);
+    myServo.write(0);
+    delay(1000);
+    pinMode(trig, OUTPUT);
+    pinMode(echo, INPUT);
+}
+
+float sensorScan(){
+    digitalWrite(trig, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trig, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trig, LOW);
+    dur = pulseIn(echo, HIGH, 38000);
+    if (dur == 0) return -1;
+    dist = dur / 58.0;
+    return dist;
+}
+
+int updateDirection(int dir, int angle){
+  if (angle >=180) return-1;
+  if (angle <= 0) return 1;
+  return dir;
+}
+
+
+void servoMove(int angle){
+  myServo.write(angle);
 }
 
 void loop() {
-  for (int theta = 0; theta <= 180; theta++) {
-    myServo.write(theta);
-    digitalWrite(trig, LOW);
-    delayMicroseconds(2);
-    digitalWrite(trig, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trig, LOW);
-    dur = pulseIn(echo, HIGH, 38000);
-    dist = dur / 58;
-    if (dur > 0) {
-    Serial.println(dist);
-    }
-    delay(100);
-  }
-  for (int theta = 180; theta >= 0; theta--) {
-    myServo.write(theta);
-    digitalWrite(trig, LOW);
-    delayMicroseconds(2);
-    digitalWrite(trig, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trig, LOW);
-    dur = pulseIn(echo, HIGH, 38000);
-    dist = dur / 58;
-    if (dur > 0) {
-    Serial.println(dist);
-    }
-    delay(100);
-  }
+  dist= sensorScan();
+  theta += sweepdir;
+  sweepdir = updateDirection(sweepdir, theta);
+  servoMove(theta);
+  Serial.println(dist);
+  delay(60);
 }
